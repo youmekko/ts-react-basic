398693bf666ec6a3da332b77352eadad09ee92a2
youmekko <youmekko.o@gmail.com>
2020-08-27 16:02:33 +0900

    TS - 리액트 useRef

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,25 +1,24 @@
 import React, { useState } from 'react'
 
-/**
- * (예제) 
- * 상태가 null 일 수도 있고 아닐 수도 있을 때는 Generic를 사용하면 좋다.
- */
-type information = { name: string, description: string}
-const [info, setInfo] = useState<information | null>(null)
-
-/**
- * (예제)
- * 추가적으로 상태의 타입이 까다로운 구조를 가진 객체이거나 배열 일 때도 Generics를 명시하는 것이 좋다.
- */
-
- type Todo = { id: number, text: string, done: boolean }
- const [todos, setTodos] = useState<Todo[]>([])
-
-
 function Counter() {
     const [count, setCount] = useState(0)
     const onIncrease = () => setCount(count + 1)
     const onDecrease = () => setCount(count - 1)
+
+    /**
+     * (예제) 
+     * 상태가 null 일 수도 있고 아닐 수도 있을 때는 Generic를 사용하면 좋다.
+     */
+    type information = { name: string, description: string}
+    const [info, setInfo] = useState<information | null>(null)
+
+    /**
+     * (예제)
+     * 추가적으로 상태의 타입이 까다로운 구조를 가진 객체이거나 배열 일 때도 Generics를 명시하는 것이 좋다.
+     */
+
+    type Todo = { id: number, text: string, done: boolean }
+    const [todos, setTodos] = useState<Todo[]>([])
     
     return (
         <div>
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import Greetings from './Greetings'
 import Counter from './Counter'
 import Counter2 from './Counter2'
 import MyForm from './MyForm'
+import MyForm2 from './MyForm2'
 import ReducerSample from './ReducerSample';
 
 const onClick = (name: string) => {
@@ -20,6 +21,7 @@ ReactDOM.render(
         <Counter />
         <Counter2 />
         <MyForm onSubmit={onSubmit}/>
+        <MyForm2 onSubmit={onSubmit}/>
         <ReducerSample />
     </React.StrictMode>,
   document.getElementById('root')
